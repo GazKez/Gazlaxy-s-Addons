aa48d3b545ae65f18c9360a2330a0b4cd05e9bf2
GazKez <55027773+GazKez@users.noreply.github.com>
2026-01-08 16:49:39 +0100

    Update run.sh

--- amule/run.sh
+++ amule/run.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bashio
+#!/usr/bin/bashio
 
-# Variables de entorno desde la configuración de HA
-export PUID=$(bashio::config 'puid' '1000')
-export PGID=$(bashio::config 'pgid' '1000')
-export TZ=$(bashio::config 'tz' 'Europe/Madrid')
+# Variables de entorno
+export PUID=$(bashio::config 'puid')
+export PGID=$(bashio::config 'pgid')
+export TZ=$(bashio::config 'tz')
 export WEBUI_PWD=$(bashio::config 'webui_pwd')
-export MOD_AUTO_RESTART_ENABLED=$(bashio::config 'mod_auto_restart_enabled' 'true')
+export MOD_AUTO_RESTART_ENABLED=$(bashio::config 'mod_auto_restart_enabled')
 
-# Directorios en /media
+if bashio::config.has_value 'gui_pwd'; then export GUI_PWD=$(bashio::config 'gui_pwd'); fi
+
+# Directorios
 echo "Preparando directorios en /media/amule..."
 mkdir -p /media/amule/incoming
 mkdir -p /media/amule/temp
 
-# Si el usuario es root (0), a veces hay problemas de permisos, 
-# pero la imagen de ngosang lo gestiona internamente con PUID/PGID
 export MOD_AUTO_SHARE_DIRECTORIES="/media/amule/incoming"
 
 echo "Lanzando aMule..."
