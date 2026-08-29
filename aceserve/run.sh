5ec99aed091a2fee6c7e244b82011e2dbb920a9d
GazKez <55027773+GazKez@users.noreply.github.com>
2026-08-29 23:54:15 +0200

    Update run.sh

---
+++ aceserve/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Starting Gazlaxy AceServe..."
+bashio::log.info "Architecture: $(uname -m)"
+
+bashio::log.info "AceServe + HTTPAceProxy initialization"
+
+# TODO: Start AceServe
+# TODO: Start HTTPAceProxy
+
+bashio::log.info "Gazlaxy AceServe container started"
+
+sleep infinity
